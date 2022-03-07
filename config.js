5fd25d95e7e6792de1f619d751479f17c2299596
tkc2110001 <101132062+tkc2110001@users.noreply.github.com>
2022-03-08 00:25:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "This web dành cho bé Dung",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bé Meo Meo',
+    introDesc: `Gửi bé Meo Meo, bé đừng giận con nha con yêu bé lắm bé giận con con buồn con khóc huhuhuhu nha nha`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    desc: 'Bé đoán xem ai yêu bé nhất nào ',
+    btnYes: 'Là con nhưng nhấn vào thì được',
+    btnNo: 'Cũng là con nhưng nhấn vào thì chịu',
+    question: 'Bé gửi lời yêu thương cho con nha, phải lời yêu thương cơ không khum chịu',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Ahihi ngại quá yêu bé nhiều mà ứ ừ',
-    messDesc: 'Bé phải yêu con nhiều nhiều nha',
+    mess: 'Yêu quá ai mà đáng yêu vậy chời yêu xỉu',
+    messDesc: '8/3 vui vẻ không buồn nha hẹn bé 25/3 rùi gặp.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
